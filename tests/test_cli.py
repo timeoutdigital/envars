timeoutdigital/envars

commit 52668e9d871091b673314b7ba3eefb28e59814ae

enable exec on one var (#15)
sometimes this is useful, slight refactor to enable

`envars exec -v AVAR <command>`

envars/models.py
@@ -68,7 +68,7 @@ class EnVar:
     def __repr__(self):
         return f"EnVar(name='{self.name}', envs={self.envs})"
 
-    def get_value(self, env, account, decrypt=False):
+    def get_value(self, env, account, decrypt=False, fetch_pstore=False):
         value = None
         if env in self.envs:
             if isinstance(self.envs[env], dict):
@@ -82,6 +82,21 @@ class EnVar:
                     value = self.decrypt(self.envs['default'][account], 'default', account, decrypt)
             else:
                 value = self.decrypt(self.envs['default'], 'default', None, decrypt)
+
+        if value and fetch_pstore and not isinstance(value, Secret):
+            if 'parameter_store:' in value:
+                pname = value.split(':')[1]
+                pvalue = 'UNKNOWN-ERROR-FETCHING-FROM-PARAMETER-STORE'
+                try:
+                    param = ssm_client.get_parameter(Name=pname)
+                    pvalue = param['Parameter']['Value']
+                except ClientError as e:
+                    if e.response['Error']['Code'] == 'ParameterNotFound':
+                        pvalue = f'NOT-FOUND-IN-PSTORE-{pname}'
+                    elif e.response['Error']['Code'] == 'AccessDeniedException':
+                        pvalue = f'PARAMETER-STORE-ACCESS-DENIED-{pname}'
+                value = pvalue
+
         return value
 
     def decrypt(self, value, env, account, decrypt):
@@ -201,6 +216,11 @@ class EnVars:
             envars[var.name] = var.envs
         return envars
 
+    def get_var(self, var, env, account):
+        for v in self.envars:
+            if v.name == var:
+                return {v.name: v.get_value(env, account, fetch_pstore=True)}
+
     def build_env(self, env, account, decrypt=False, template_vars=None):
         logging.debug(f'build_env({env}, {account})')
         envars = {}
@@ -210,7 +230,7 @@ class EnVars:
         template_vars['STAGE'] = env
         # fetch all the non secret values
         for v in self.envars:
-            value = v.get_value(env, account)
+            value = v.get_value(env, account, fetch_pstore=True)
             if value and not isinstance(value, Secret):
                 if v.name not in template_vars.keys():
                     template_vars[v.name] = value
@@ -228,22 +248,6 @@ class EnVars:
             if value:
                 if v.name not in envars:
                     envars[v.name] = value
-
-        # process 'parameter_store' values
-        for var in envars:
-            if not isinstance(envars[var], Secret):
-                if 'parameter_store:' in envars[var]:
-                    pname = envars[var].split(':')[1]
-                    pvalue = 'UNKNOWN-ERROR-FETCHING-FROM-PARAMETER-STORE'
-                    try:
-                        param = ssm_client.get_parameter(Name=pname)
-                        pvalue = param['Parameter']['Value']
-                    except ClientError as e:
-                        if e.response['Error']['Code'] == 'ParameterNotFound':
-                            pvalue = f'NOT-FOUND-IN-PSTORE-{pname}'
-                        elif e.response['Error']['Code'] == 'AccessDeniedException':
-                            pvalue = f'PARAMETER-STORE-ACCESS-DENIED-{pname}'
-                    envars[var] = pvalue
 
         return envars
 

tests/test_cli.py
@@ -1,8 +1,10 @@
+import os
 import subprocess
+from unittest.mock import MagicMock
 
 import yaml
 
-from envars.envars import add_var, process
+from envars import envars
 
 CMD = 'python -m envars.envars'
 
@@ -93,7 +95,7 @@ def test_eqauls_in_value(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Args', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -103,7 +105,7 @@ def test_eqauls_in_value(tmp_path):
         'yaml': False,
         'decrypt': True,
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == ['TEST1=abc=']
 
@@ -118,7 +120,7 @@ def test_two_env_vars_returned(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
     args = type('Args', (object,), {
         'variable': 'TEST2=B',
         'secret': False,
@@ -127,7 +129,7 @@ def test_two_env_vars_returned(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Args', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -137,7 +139,7 @@ def test_two_env_vars_returned(tmp_path):
         'yaml': False,
         'decrypt': True,
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == ['TEST1=A', 'TEST2=B']
 
@@ -152,7 +154,7 @@ def test_template_var(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
     args = type('Args', (object,), {
         'variable': 'HOSTNAME=test.{{ DOMAIN }}',
         'secret': False,
@@ -161,7 +163,7 @@ def test_template_var(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Args', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -171,7 +173,7 @@ def test_template_var(tmp_path):
         'yaml': False,
         'decrypt': True,
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == ['DOMAIN=timeout.com', 'HOSTNAME=test.timeout.com']
 
@@ -186,7 +188,7 @@ def test_extra_template_passing(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Args', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -196,7 +198,7 @@ def test_extra_template_passing(tmp_path):
         'decrypt': True,
         'template_var': ['RELEASE=12324523523523525234523523'],
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == ['RELEASE=12324523523523525234523523']
 
@@ -212,7 +214,7 @@ def test_yaml_print_env(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
     args = type('Args', (object,), {
         'variable': 'TEST=test',
         'secret': False,
@@ -221,7 +223,7 @@ def test_yaml_print_env(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
     args = type('Args', (object,), {
         'variable': 'STEST=stest',
         'secret': False,
@@ -230,7 +232,7 @@ def test_yaml_print_env(tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Args', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -241,7 +243,7 @@ def test_yaml_print_env(tmp_path):
         'decrypt': True,
         'template_var': ['RELEASE=12324523523523525234523523'],
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == "envars:\n  RELEASE: '12324523523523525234523523'\n  TEST: test\n"
 
@@ -264,7 +266,7 @@ def test_secret(kms_stub, tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
     args = type('Arg', (object,), {
         'filename': f'{tmp_path}/envars.yml',
         'env': 'prod',
@@ -273,7 +275,7 @@ def test_secret(kms_stub, tmp_path):
         'yaml': False,
         'decrypt': True,
     })
-    ret = process(args)
+    ret = envars.process(args)
 
     assert ret == ['TEST=sssssh']
 
@@ -292,7 +294,7 @@ def test_parameter_store_value(ssm_stub, tmp_path):
         'desc': None,
         'account': None,
     })
-    add_var(args)
+    envars.add_var(args)
 
     args = type('Arg', (object,), {
         'filename': f'{tmp_path}/envars.yml',
@@ -302,5 +304,41 @@ def test_parameter_store_value(ssm_stub, tmp_path):
         'yaml': False,
         'decrypt': False,
     })
-    ret = process(args)
+    ret = envars.process(args)
     assert ret == ['PTEST=1234']
+
+
+def test_exec_one_var(tmp_path):
+    # used by some instance service scripts
+    run_cmd(tmp_path, 'init --app testapp --environments prod,staging --kms-key-arn abc')
+    args = type('Args', (object,), {
+        'variable': 'TEST=test',
+        'secret': False,
+        'filename': f'{tmp_path}/envars.yml',
+        'env': 'default',
+        'desc': None,
+        'account': None,
+    })
+    envars.add_var(args)
+    args = type('Args', (object,), {
+        'variable': 'STEST=stest',
+        'secret': False,
+        'filename': f'{tmp_path}/envars.yml',
+        'env': 'default',
+        'desc': None,
+        'account': None,
+    })
+    envars.add_var(args)
+
+    args = type('Args', (object,), {
+        'account': None,
+        'command': ['printenv'],
+        'env': 'prod',
+        'filename': f'{tmp_path}/envars.yml',
+        'var': 'TEST',
+    })
+    envars.os.execlp = MagicMock()
+    envars.execute(args)
+
+    assert os.environ.get('TEST') == 'test'
+    assert 'STEST' not in os.environ

PYTHON_VERSION
@@ -1,0 +1,1 @@
+3.10.10

envars/envars.py
@@ -154,6 +154,12 @@ def main():
         required=False,
     )
     parser_exec.add_argument(
+        '-v',
+        '--var',
+        required=False,
+        default=None,
+    )
+    parser_exec.add_argument(
         '-a',
         '--account',
         required=False,
@@ -192,7 +198,6 @@ def execute(args):
     command = args.command
     args.yaml = False
     args.decrypt = True
-    args.var = None
     if not args.env:
         args.env = os.environ.get('STAGE')
     if not args.env:
@@ -200,10 +205,18 @@ def execute(args):
         sys.exit(1)
     if 'RELEASE_SHA' in os.environ:
         args.template_var = [f'RELEASE={os.environ.get("RELEASE_SHA")}']
-    ret = process(args)
+
     vals = {}
-    for val in ret:
-        vals[val.split('=')[0]] = val.split('=')[1]
+    if args.var:
+        envars = EnVars(args.filename)
+        envars.load()
+        vals = envars.get_var(args.var, args.env, args.account)
+    else:
+        args.var = None
+        ret = process(args)
+        for val in ret:
+            vals[val.split('=')[0]] = val.split('=')[1]
+
     os.environ.update(vals)
     os.execlp(command[0], *command)
 

commit ee693260e968345a38e967cf25090731d70980f4

TOPS-656 - fix STAGING template var in pstore values (#22)
broken in
https://github.com/timeoutdigital/envars/commit/52668e9d871091b673314b7ba3eefb28e59814ae

envars/models.py
@@ -82,6 +82,8 @@ class EnVar:
                 from .ssm import SsmAgent
                 ssm_agent = SsmAgent()
                 pname = value.split(':')[1]
+                jenv = jinja2.Environment()
+                pname = jenv.from_string(pname).render({'STAGE': env})
                 value = ssm_agent.fetch(pname)
 
         return value

tests/test_cli.py
@@ -289,11 +289,42 @@ def test_secret(kms_stub, tmp_path):
 def test_parameter_store_value(ssm_stub, tmp_path):
     ssm_stub.add_response(
         'get_parameter',
-        service_response={'Parameter': {'Value': '1234'}}
+        service_response={'Parameter': {'Value': '1234'}},
+        expected_params={'Name': '/gp-web/prod/1234/CANARY', 'WithDecryption': True},
     )
     run_cmd(tmp_path, 'init --app testapp --environments prod,staging --kms-key-arn abc')
     args = type('Arg', (object,), {
         'variable': 'PTEST=parameter_store:/gp-web/prod/1234/CANARY',
+        'secret': False,
+        'filename': f'{tmp_path}/envars.yml',
+        'env': 'default',
+        'desc': None,
+        'account': None,
+    })
+    envars.add_var(args)
+
+    args = type('Arg', (object,), {
+        'filename': f'{tmp_path}/envars.yml',
+        'env': 'prod',
+        'account': None,
+        'template_var': ['RELEASE=1234'],
+        'yaml': False,
+        'decrypt': False,
+        'quote': False
+    })
+    ret = envars.process(args)
+    assert ret == ['PTEST=1234']
+
+
+def test_stage_template_parameter_store_value(ssm_stub, tmp_path):
+    ssm_stub.add_response(
+        'get_parameter',
+        service_response={'Parameter': {'Value': '1234'}},
+        expected_params={'Name': '/gp-web/prod/1234/CANARY', 'WithDecryption': True},
+    )
+    run_cmd(tmp_path, 'init --app testapp --environments prod,staging --kms-key-arn abc')
+    args = type('Arg', (object,), {
+        'variable': 'PTEST=parameter_store:/gp-web/{{ STAGE }}/1234/CANARY',
         'secret': False,
         'filename': f'{tmp_path}/envars.yml',
         'env': 'default',